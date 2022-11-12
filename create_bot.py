95cce25f518d950231c711d847594f4ca4465f3f
Лычагин Сергей <serg.programmer777@gmail.com>
2022-11-12 11:11:02 +0300

    Add gitignore and requirements

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
+load_dotenv(override=True)
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=os.environ.get('TOKEN'))
 dp = Dispatcher(bot, storage=storage)
